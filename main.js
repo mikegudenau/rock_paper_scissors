0451ddca339e866839d3c5976125a85f20672334
Mike Gudenau <mikegudenau@gmail.com>
2023-04-20 11:05:42 -0700

    Render selected data model fighters to the DOM

--- main.js
+++ main.js
@@ -6,30 +6,50 @@ var wood = document.querySelector("#wood");
 var metal = document.querySelector("#metal");
 var introChoose = document.querySelector(".intro-choose");
 var fighterChoose = document.querySelector(".fighter-choose");
-var humanToken = document.querySelector(".human-token")
-var humanName = document.querySelector(".human-name")
-var computerToken = document.querySelector(".computer-token")
-var computerName = document.querySelector(".computer-name")
+var humanToken = document.querySelector(".human-token");
+var humanName = document.querySelector(".human-name");
+var computerToken = document.querySelector(".computer-token");
+var computerName = document.querySelector(".computer-name");
+var playerContainer = document.querySelectorAll(".player-container")
+
+console.log(playerContainer[0]);
 
 var playerChoices = ["metal", "earth", "wood", "water", "fire"];
 
-function createPlayer(name, token) {
-  var player = {
-    name: name,
-    token: token,
-    wins: 0,
-  };
-  return player;
+// Event Listeners
+window.addEventListener("load", loadPage);
+
+function createPlayer(humanToken, computerToken) {
+  var players = [
+    {
+      name: "Human",
+      token: humanToken,
+      wins: 0,
+    },
+    {
+      name: "Computer",
+      token: computerToken,
+      wins: 0,
+    },
+  ];
+  return players;
 }
 
-console.log(createPlayer('🤵🏻‍♂️', 'Human'));
+function loadPage() {
+  var players = createPlayer("🤵🏻‍♂️", "🎮");
+  for (var i = 0; i < playerContainer.length; i++) {
+    playerContainer[i].innerHTML = `
+    <p class="token" role="img" aria-label="human">${players[i].token}</p>
+    <h3 class="name">${players[i].name}</h3>
+    <p>Wins: <span id="score">${players[i].wins}</span></p>`
+  }
+}
 
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
 
 // Game
-
 function createGame(user, computer) {
   if (determineTie(user, computer)) {
     console.log("It's a tie!");
@@ -43,7 +63,6 @@ function createGame(user, computer) {
 createGame("earth", playerChoices[getRandomIndex(playerChoices)]);
 
 // Game Conditions
-
 function determineTie(user, computer) {
   if (user === computer) {
     return true;
@@ -63,7 +82,7 @@ function determineWinner(user, computer) {
   return winCombinations[user].includes(computer);
 }
 
-console.log(determineTie('fire', 'water'));
+console.log(determineTie("fire", "water"));
 
 function showDOMElement(element) {
   element.classList.remove("hidden");
